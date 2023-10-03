12ab7e22f699a44b8d673cbef3832dd10db1df16
Abraham Ayamigah <abkc070@gmail.com>
2023-10-03 07:19:24 -0400

    hello world

---
+++ variables/hello-world.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+######################################
+# Author: Abraham Ayamigah
+# Version: v1.0.0
+# Date: 29th September, 2023
+# Usage: ./hello-world.sh
+######################################
+
+# creating a variable
+hello_text="Hello Wold!"
+
+#Print the str to the terminal
+echo ${hello_text}
+
